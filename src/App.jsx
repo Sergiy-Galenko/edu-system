c74fb098b42d4a24c577ee93a216ac18f3a67be5
kokush <witalik.logoyda@gmail.com>
2024-12-26 21:41:51 +0200

    [feat] add title bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import SideBarMenu from "./components/SideBarMenu";
+import TitleBar from "./components/TitleBar";
+import { navBarData, titleBar } from "./data/content";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<TitleBar title={titleBar.title} subTitle={titleBar.subTitle} logo={titleBar.logo} />
+			<SideBarMenu navBarData={navBarData} />
+		</div>
+	);
 }
 
 export default App;
